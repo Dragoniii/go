8d5390f9bd459cae72771a7999163485d0aa45df
Joel Luis Steffen <jo.lsteffen@gmail.com>
2024-01-16 14:23:34 -0300

    ok

---
+++ 007-learning-the-go-standard-library/01 fmt package/Start/print/print_start.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// TODO: Print a simple string, no newline
+	fmt.Print("Welcome to Go!")
+
+	// TODO: Print with a newline
+	fmt.Println("This string ends with a new line")
+
+	// TODO: Print a string with values
+	const answer = 42
+
+	// TODO: Print a string with multiple interspersed values
+	const a, b, c = 5, 5, 10
+
+	// TODO: print a slice of data
+	//items := []int{10, 20, 40, 80}
+
+}
