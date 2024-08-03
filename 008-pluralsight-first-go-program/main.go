34ec90d5cff1cd32596005aab68446ac5bcdb016
Joel Luis Steffen <jo.lsteffen@gmail.com>
2024-08-02 21:19:58 -0300

    ok

--- 008-pluralsight-first-go-program/main.go
+++ 008-pluralsight-first-go-program/main.go
@@ -98,3 +98,16 @@
 // }
 // -//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { fmt.Fprint(w, "Hello") })
+
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "./home.html") })
+
+	http.ListenAndServe(":3000", nil)
+}
