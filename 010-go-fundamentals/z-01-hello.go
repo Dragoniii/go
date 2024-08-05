214067b467f34281695b9dcb067532df89ec7ecc
Joel Luis Steffen <jo.lsteffen@gmail.com>
2024-08-05 11:32:37 -0300

    strings

---
+++ 010-go-fundamentals/z-01-hello.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func printHelloWorld() {
+	fmt.Println(" 01 - Hello, \n World!")
+	fmt.Println(" 01 - Hello, World!")
+	fmt.Println(
+	`01 - 
+	Hello, 
+	World!`)
+	fmt.Println(` 01 - Hello, \n World!`)
+}
